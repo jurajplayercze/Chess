f1829404c84e007127c22a8ce81b4e39891a11ef
Jiří Veselý <jurajplayercze@gmail.com>
2024-05-21 15:20:32 +0200

    final-v3

---
+++ figures.py
@@ -0,0 +1,184 @@
+class Figure:
+    possible_moves = []
+    color = "black"
+    image = None
+    def __init__(self,color):
+        self.color = color
+    def get_possible_moves(self,coords: list, board: list):
+        moves = []
+        #Remove cells out of bounds
+        for cell in moves:
+            if (cell[0] or cell[1]) > 7 or (cell[0] or cell[1]) < 0:
+                moves.remove(cell)      
+        return moves
+
+class Pawn(Figure):
+    image = "Chess/images/-color_pawn.png"
+    def get_all_special_attacks(self, coords: list, board: dict):
+        moves = []
+        row, col = coords
+        if self.color == "white":
+            #Check if figure can go diagonally
+            moves.append((row+1,col+1))
+            moves.append((row+1,col-1))
+        else:
+            moves.append((row-1,col+1))
+            moves.append((row-1,col-1))
+        return moves
+    def get_special_attacks(self,coords: list, board: dict):
+        moves = []
+        row, col = coords
+        if self.color == "white":
+            #Check if figure can go diagonally
+            if board.get((row+1,col+1)) != None and board.get((row+1,col+1)).color == "black":
+                moves.append((row+1,col+1))
+            if board.get((row+1,col-1)) != None and board.get((row+1,col-1)).color == "black":
+                moves.append((row+1,col-1))
+        else:
+            if board.get((row-1,col+1)) != None and board.get((row-1,col+1)).color == "white":
+                moves.append((row-1,col+1))
+            if board.get((row-1,col-1)) != None and board.get((row-1,col-1)).color == "white":
+                moves.append((row-1,col-1))
+        return moves
+
+    def get_possible_moves(self, coords: list, board: dict):
+        moves = []
+        row, col = coords
+        if self.color == "white":
+            #Check if figure can go diagonally
+            if (row == 1) and (board.get((row+1,col)) == None) and (board.get((row+2,col)) == None):
+                moves.append((row+2,col))
+            if board.get((row+1,col)) == None:
+                moves.append((row+1,col))
+        else:
+            if (row == 6) and (board.get((row-1,col)) == None) and (board.get((row-2,col)) == None):
+                moves.append((row-2,col))
+            if board.get((row-1,col)) == None:
+                moves.append((row-1,col))
+        moves.extend(self.get_special_attacks(coords,board))
+        return moves
+
+class King(Figure):
+    possible_moves = [[0,1],[1,0],[1,1],[-1,1]]
+    image = "Chess/images/-color_king.png"
+    def get_possible_moves(self, coords, board: dict):
+        king_moves = []
+        row, col = coords
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+        for x, y in directions:
+            new_row, new_col = row + x, col + y
+            # Is within board
+            if (0 <= new_row <= 7) and (0 <= new_col <= 7):
+                if board.get((new_row, new_col)) is None:
+                    king_moves.append((new_row, new_col))
+                elif board.get((new_row, new_col)).color != self.color:
+                    king_moves.append((new_row, new_col))
+        #Remove possible check cells
+        for cell in board:
+            #Get figure at coords
+            figure = board.get(cell)
+            #Is figure, is not same color, is not King
+            if isinstance(figure,Figure) and figure.color != self.color and not isinstance(figure,King):#Upravit i pro kinga!!!
+                #Get possible moves of the figure
+                if not isinstance(figure,Pawn):
+                    for possible_move in figure.get_possible_moves(cell,board):
+                        #If moves of king contains possible move of the figure
+                        if possible_move in king_moves:
+                            #Remove move from kings moves
+                            king_moves.remove(possible_move)
+                else:
+                    for pawn_attack in figure.get_all_special_attacks(cell,board).copy():
+                        if pawn_attack in king_moves:
+                            king_moves.remove(pawn_attack)
+        return king_moves
+
+class Queen(Figure):
+    image = "Chess/images/-color_queen.png"
+    def get_possible_moves(self, coords, board: dict):
+        moves = []
+        row, col = coords
+        directions = [[1, 1], [1, -1], [-1, 1], [-1, -1], [0, 1], [0, -1], [1, 0], [-1, 0]]
+        for x, y in directions:
+            new_row, new_col = row, col
+            while True:
+                new_row += x
+                new_col += y
+                # Is within board
+                if (0 <= new_row <= 7) and (0 <= new_col <= 7):
+                    if board.get((new_row, new_col)) == None:
+                        moves.append((new_row, new_col))
+                    elif board.get((new_row, new_col)).color != self.color:
+                        moves.append((new_row, new_col))
+                        break
+                    else:
+                        break
+                else:
+                    break
+        return moves
+
+class Bishop(Figure):
+    image = "Chess/images/-color_bishop.png"
+    def get_possible_moves(self, coords, board: dict):
+        moves = []
+        row, col = coords
+        directions = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+        for x, y in directions:
+            new_row, new_col = row, col
+            while True:
+                new_row += x
+                new_col += y
+                # Is within board
+                if (0 <= new_row <= 7) and (0 <= new_col <= 7):
+                    if board.get((new_row, new_col)) == None:
+                        moves.append((new_row, new_col))
+                    elif board.get((new_row, new_col)).color != self.color:
+                        moves.append((new_row, new_col))
+                        break
+                    else:
+                        break
+                else:
+                    break
+        return moves
+
+class Knight(Figure):
+    moves_between_coords = False
+    image = "Chess/images/-color_knight.png"
+    def get_possible_moves(self, coords, board: dict):
+        moves = []
+        row, col = coords
+        directions = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
+        for x, y in directions:
+            new_row, new_col = row + x, col + y
+            #Is within board
+            if (new_row >= 0 and new_row <= 7) and (new_col >= 0 and new_col <= 7):
+                #Is empty
+                if board.get((new_row, new_col)) == None:
+                    moves.append((new_row, new_col))
+                #Is enemy
+                elif board.get((new_row, new_col)).color != self.color:
+                    moves.append((new_row, new_col))
+        return moves
+
+class Rook(Figure):
+    image = "Chess/images/-color_rook.png"
+    def get_possible_moves(self, coords, board: dict):
+        moves = []
+        row, col = coords
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        for x, y in directions:
+            new_row, new_col = row, col
+            while True:
+                new_row += x
+                new_col += y
+                # Is within board
+                if (0 <= new_row <= 7) and (0 <= new_col <= 7):
+                    if board.get((new_row, new_col)) is None:
+                        moves.append((new_row, new_col))
+                    elif board.get((new_row, new_col)).color != self.color:
+                        moves.append((new_row, new_col))
+                        break
+                    else:
+                        break
+                else:
+                    break
+        return moves
---
+++ main.py
@@ -0,0 +1,267 @@
+import pygame
+from figures import Rook,Knight,Bishop,Queen,King,Pawn,Figure
+
+#-Options------------------------------------------------
+RESOLUTION = 600,600
+CELL_SIZE = 75
+#--------------------------------------------------------
+
+BOARD = {}
+END = False
+COLOR_TURN = "white"
+SELECTED_FIGURE = None
+SELECTED_FIGURE_COORDS = None
+
+def set_figure(screen: pygame.Surface, figure: Figure, coords):
+    BOARD[coords] = figure
+    i = (str(figure.image)).replace("-color",figure.color)
+    i = pygame.image.load(i)
+    image = pygame.transform.scale(i, (CELL_SIZE,CELL_SIZE))
+    screen.blit(image, (coords[1] * CELL_SIZE, (7 - coords[0]) * CELL_SIZE))
+
+def reset_color_cell(coords):
+    pygame.draw.rect(screen, ("gray30" if (coords[0] + coords[1]) % 2 != 0 else "white"), (coords[1] * CELL_SIZE, (7 - coords[0]) * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+def draw_ending_screen(loser):
+    if COLOR_TURN == "white":
+        winner = "black"
+    else:
+        winner = "white"
+    pygame.draw.rect(screen, "chartreuse4", ((RESOLUTION[0] // 2 - RESOLUTION[0] // 6), (RESOLUTION[1] // 2 - RESOLUTION[1] // 6), (RESOLUTION[0] // 3), (RESOLUTION[0] // 3)))
+    font = pygame.font.Font(None,40)
+    text_window = font.render("Winner: " + winner, True, ("black" if winner == "black" else "white"))
+    text_rect = text_window.get_rect()
+    text_rect.center = (RESOLUTION[0] // 2, RESOLUTION[1] // 2)
+    screen.blit(text_window, text_rect)
+
+def reset_selection():
+    global SELECTED_FIGURE, SELECTED_FIGURE_COORDS
+    SELECTED_FIGURE = None
+    SELECTED_FIGURE_COORDS = None
+
+def init_board(screen):
+    for col, piece in enumerate([Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]):
+        BOARD[(0, col)] = piece("white")
+        set_figure(screen, piece("white"), (0, col))
+    for col in range(8):
+        BOARD[(1, col)] = Pawn("white")
+        set_figure(screen, Pawn("white"), (1, col))
+    for col, piece in enumerate([Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]):
+        BOARD[(7, col)] = piece("black")
+        set_figure(screen, piece("black"), (7, col))
+    for col in range(8):
+        BOARD[(6, col)] = Pawn("black")
+        set_figure(screen, Pawn("black"), (6, col))
+    for row in range(2, 6):
+        for col in range(8):
+            BOARD[(row, col)] = None
+
+def is_same_color(color,coords):
+    cell = BOARD.get(tuple(coords))
+    if isinstance(cell,Figure) and cell.color == color:
+        return True
+    return False
+#------Kings section---------------------------------------------------
+def get_king_coords(color):
+    for cell in BOARD:
+        if isinstance(BOARD.get(cell),King):
+            if BOARD.get(cell).color == color:
+                return cell
+
+def is_king_in_check(king_color: str, board = BOARD):
+    king_position = get_king_coords(king_color)
+    #Loop thru cells
+    for coords, figure in board.items():
+        #If cell is figure and figure is not same color as king
+        if (figure != None) and (figure.color != king_color):
+            #Get possible moves of the figure
+            possible_moves = figure.get_possible_moves(coords, board)
+            #If king is in possible moves of the figure
+            if king_position in possible_moves:
+                return True
+    return False
+
+#Return true, if king is going to be in check
+def is_going_to_be_check(king_color: str, new_king_coords, board):
+    #Get old coords of king
+    old_king_coords = get_king_coords(king_color)
+    #Get copy of board
+    shadow_board = board.copy()
+    shadow_board[old_king_coords] = None
+    shadow_board[new_king_coords] = King(king_color)
+    #Loop thru cells
+    for cell in shadow_board:
+        figure = shadow_board.get(cell)
+        #If cell is figure and figure is not same color as king
+        if isinstance(figure, Figure) and figure.color != king_color:
+            #If coords of king is in possible moves of the figure
+            if new_king_coords in figure.get_possible_moves(cell, shadow_board):
+                return True
+    return False
+
+#Function that finds figure which checks king argument and returns it's coords
+def get_checking_figure_coords(king_color,board):
+    king = get_king_coords(king_color)
+    for cell in board:
+        figure = board.get(cell)
+        #Is enemy figure
+        if isinstance(figure, Figure) and figure.color != king_color:
+            #Is checking king
+            for move in figure.get_possible_moves(cell, board):
+                if move == king:
+                    return cell
+    return None
+
+def check_for_checkmate():
+    for king_color in ["white", "black"]:
+        #If looped king is checked
+        if is_king_in_check(king_color):
+            moves = []
+            king_coords = get_king_coords(king_color)
+            checking_figure_coords = get_checking_figure_coords(king_color,BOARD)
+            checking_figure = BOARD.get(checking_figure_coords)
+            checking_figure_moves = checking_figure.get_possible_moves(checking_figure_coords,BOARD)
+            #Count valid moves of all figures of same color
+            for cell in BOARD:
+                figure = BOARD.get(cell)
+                if isinstance(figure, Figure) and figure.color == king_color:
+                    valid_moves = []
+                    valid_moves = get_valid_moves(figure,cell)
+                    moves.append(valid_moves)
+            if all(not moves for moves in moves):
+                END = True
+                draw_ending_screen(COLOR_TURN)
+#----------------------------------------------------------------------
+
+def get_valid_moves(SELECTED_FIGURE: Figure, SELECTED_FIGURE_COORDS):
+    moves = []
+    moves = (SELECTED_FIGURE.get_possible_moves(SELECTED_FIGURE_COORDS,BOARD)).copy()
+    #If TURN king is in check
+    if is_king_in_check(COLOR_TURN):
+        moves = []
+        checking_figure_coords = get_checking_figure_coords(COLOR_TURN,BOARD)
+        checking_figure = BOARD.get(checking_figure_coords)
+        checking_figure_moves = checking_figure.get_possible_moves(checking_figure_coords,BOARD)
+        #Check if selected figure can block check
+        for friendly_move in SELECTED_FIGURE.get_possible_moves(SELECTED_FIGURE_COORDS,BOARD):
+            if friendly_move in checking_figure_moves:
+                shadow_board = BOARD.copy()
+                shadow_board[friendly_move] = SELECTED_FIGURE
+                if not is_going_to_be_check(COLOR_TURN,get_king_coords(COLOR_TURN),shadow_board):
+                    moves.append(friendly_move)
+        #Check for special pawn attack
+        if isinstance(SELECTED_FIGURE,Pawn):
+            if checking_figure_coords in SELECTED_FIGURE.get_all_special_attacks(SELECTED_FIGURE_COORDS,BOARD):
+                moves.append(checking_figure_coords)
+        #If figure can takeout checking figure
+        elif checking_figure_coords in SELECTED_FIGURE.get_possible_moves(SELECTED_FIGURE_COORDS,BOARD):
+            moves.append(checking_figure_coords)
+        #Simulate all possible moves
+        for move in moves.copy():
+            shadow_board = BOARD.copy()
+            shadow_board[move] = SELECTED_FIGURE
+            shadow_board[SELECTED_FIGURE_COORDS] = None
+            if get_checking_figure_coords(COLOR_TURN,shadow_board) != None:
+                moves.remove(move)
+        #If possible moves of selected figure is in checking figure moves
+        if isinstance(SELECTED_FIGURE,King):
+            for move in SELECTED_FIGURE.get_possible_moves(SELECTED_FIGURE_COORDS,BOARD):
+                print("2",move)
+                shadow_board = BOARD.copy()
+                shadow_board[SELECTED_FIGURE_COORDS] = None
+                shadow_board[move] = SELECTED_FIGURE
+                if is_king_in_check(COLOR_TURN,shadow_board):
+                    print("Appending move")
+                    moves.append(move)
+    return list(moves)
+
+def show_possible_moves(valid_moves):
+    for pos in valid_moves:
+        temp_surface = pygame.Surface((CELL_SIZE, CELL_SIZE), pygame.SRCALPHA)
+        temp_surface.fill((255, 0, 0, 75))
+        screen.blit(temp_surface, (pos[1] * CELL_SIZE, (7 - pos[0]) * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+def highlight_selected_figure(bool,coords):
+    if SELECTED_FIGURE != None:
+        if bool == True:
+            temp_surface = pygame.Surface((CELL_SIZE, CELL_SIZE), pygame.SRCALPHA)
+            temp_surface.fill((0, 255, 0, 75))
+            screen.blit(temp_surface, (coords[1] * CELL_SIZE, (7 - coords[0]) * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+        else:
+            reset_color_cell(coords)
+            set_figure(screen,BOARD.get(coords),coords)
+
+def hide_possible_moves():
+    for pos in get_valid_moves(SELECTED_FIGURE,SELECTED_FIGURE_COORDS).copy():
+        if BOARD[(pos[0],pos[1])] == None:
+            reset_color_cell((pos[0],pos[1]))
+        else:
+            reset_color_cell((pos[0],pos[1]))
+            set_figure(screen,BOARD[(pos[0],pos[1])],(pos[0],pos[1]))
+
+def draw_board():
+    for row in range(8):
+        for col in range(8):
+            x = col * CELL_SIZE
+            y = row * CELL_SIZE
+            if (row + col) % 2 != 0:
+                pygame.draw.rect(screen, "white", (x, y, CELL_SIZE, CELL_SIZE))
+            else:
+                pygame.draw.rect(screen, "gray30", (x, y, CELL_SIZE, CELL_SIZE))
+
+def switch_turn():
+    global COLOR_TURN
+    if COLOR_TURN == "white":
+        COLOR_TURN = "black"
+    else:
+        COLOR_TURN = "white"
+
+pygame.init()
+screen = pygame.display.set_mode((RESOLUTION))
+pygame.display.set_caption("Chess")
+clock = pygame.time.Clock()
+screen.fill("black")
+draw_board()
+init_board(screen)
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            # Převrácení osy Y a X(změna znaménka)
+            y = RESOLUTION[0] - y
+            clicked_cell_coords = (y // CELL_SIZE, x // CELL_SIZE)
+            clicked_figure = BOARD.get(clicked_cell_coords)
+            if (SELECTED_FIGURE == None) and (clicked_figure != None):
+                if (COLOR_TURN == clicked_figure.color):
+                    SELECTED_FIGURE = BOARD.get(clicked_cell_coords)
+                    SELECTED_FIGURE_COORDS = clicked_cell_coords
+                    show_possible_moves(get_valid_moves(SELECTED_FIGURE,SELECTED_FIGURE_COORDS).copy())
+                    highlight_selected_figure(True,SELECTED_FIGURE_COORDS)
+            elif (SELECTED_FIGURE != None) and (clicked_cell_coords != SELECTED_FIGURE_COORDS):
+                if clicked_cell_coords in get_valid_moves(SELECTED_FIGURE, SELECTED_FIGURE_COORDS):                    
+                    hide_possible_moves()
+                    reset_color_cell(clicked_cell_coords)
+                    highlight_selected_figure(False,SELECTED_FIGURE_COORDS)
+                    set_figure(screen, SELECTED_FIGURE, clicked_cell_coords)
+                    BOARD[SELECTED_FIGURE_COORDS] = None
+                    BOARD[clicked_cell_coords] = SELECTED_FIGURE
+                    reset_color_cell(SELECTED_FIGURE_COORDS)
+                    SELECTED_FIGURE = None
+                    SELECTED_FIGURE_COORDS = None
+                    switch_turn()
+                    check_for_checkmate()
+            elif (clicked_cell_coords == SELECTED_FIGURE_COORDS):
+                highlight_selected_figure(False,SELECTED_FIGURE_COORDS)
+                hide_possible_moves()
+                reset_selection()
+    pygame.display.update()
+    pygame.display.flip()
+    pygame.display
+    clock.tick(60)
+pygame.quit()
